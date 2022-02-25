6e2571333206642a9541bf2ce4e4b5e5536c7214
emmanuel <blimanuevil@gmail.com>
2022-02-25 23:38:06 +0100

    ecriture du code pour le carousel mobile

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,28 @@ const burgerMenu = document.getElementById('burger-menu');
 const Lightbox = document.getElementById('lightbox');
 const navbar = document.getElementById('navbar');
 const navbarClose = document.getElementById('navbar-close');
+const carousel = document.getElementById('carousel').childNodes;
+const carouselControls = document.querySelectorAll('.carousel-control');
+let carouselPosition = 1;
+const carouselImgs = [];
+
+carousel.forEach((el) => {
+    if(el.nodeName.toLowerCase() == 'img'){
+        carouselImgs.push(el);
+    }
+});
 
 burgerMenu.addEventListener('click', showSideMenu);
 Lightbox.addEventListener('click', hideSideMenu);
 navbarClose.addEventListener('click', hideSideMenu);
+carouselControls.forEach((el) => {
+    if(el.classList.toggle){
+        el.addEventListener('click', carouselImgNext);
+    } else {
+        el.addEventListener('click', carouselImgPrev);
+    }
+});
+ 
 
 function showSideMenu(){
     Lightbox.classList.add('show');
@@ -14,4 +32,26 @@ function showSideMenu(){
 function hideSideMenu(){
     Lightbox.classList.remove('show');
     navbar.classList.remove('menu-mobile');
+}
+function carouselImgNext(){
+    if(carouselPosition < carouselImgs.length){
+        carouselPosition++;
+    } else {
+        carouselPosition=1
+    }
+    carouselImgs.forEach((el) => {
+        el.classList.remove('show');
+    })
+    carouselImgs[carouselPosition-1].classList.add('show');
+}
+function carouselImgPrev(){
+    if(carouselPosition > 1){
+        carouselPosition--;
+    } else {
+        carouselPosition=carouselImgs.length
+    }
+    carouselImgs.forEach((el) => {
+        el.classList.remove('show');
+    })
+    carouselImgs[carouselPosition-1].classList.add('show');
 }
